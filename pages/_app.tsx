a6dbf5c11348b6e6ff1b3ac385bf2c954d67996c
Paul-Renaud Raymond <46355559+prray305@users.noreply.github.com>
2025-05-13 09:38:42 -0400

    mixpanel steup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import './globals.css';
+import { useEffect } from 'react';
+import mixpanel from 'mixpanel-browser';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Initialize Mixpanel only once when the app loads
+    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN || '', {
+      debug: process.env.NODE_ENV === 'development',
+      track_pageview: true,
+      persistence: 'localStorage',
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
